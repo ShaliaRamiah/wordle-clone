5ed560c6342a7851f55bea15eacbd981a66b9944
ShaliaRamiah <shalia.r20@gmail.com>
2023-04-06 17:51:13 +0200

    second commit

--- wordle.js
+++ wordle.js
@@ -1,6 +1,10 @@
 // The code below selects the HTML elements with the class names 'tiles-container' and 'keyboard-container'.
 const tileGrid = document.querySelector('.tiles-container');
 const keyboardLayout = document.querySelector('.keyboard-container');
+const msgDisplay = document.querySelector('.message-container');
+
+
+const word = 'AUDIT'
 
 // creating an array 'keyboardKeys' that stores strings of letters, ENTER, and DEL.
 const keyboardKeys = [
@@ -45,6 +49,10 @@ const inputRows = [
 
 ]
 
+let activeRow = 0;
+let activeTile = 0;
+let endGame = false;
+
 // The code below iterates through each row in 'inputRows' and creates a div element with an ID, and then appends
 inputRows.forEach((inputRow, inputRowIndex) => {
   const rowElement = document.createElement('div')
@@ -52,15 +60,12 @@ inputRows.forEach((inputRow, inputRowIndex) => {
   inputRow.forEach((input, inputIndex) => {
     const tileElement = document.createElement('div')
     tileElement.setAttribute('id', 'inputRow-' + inputRowIndex + '-tiles-' + inputIndex)
+    //line below displays the tiles
+    tileElement.classList.add('tiles')
     rowElement.append(tileElement)
   })
   tileGrid.append(rowElement)
 })
-
-// The code below defines a function 'buttonClick' that logs 'clicked' to the console.
-const buttonClick = () => {
-  console.log('clicked');
-}
 
 // The code below iterates through each key in the 'keyboardKeys' array and creates a button element with the key value as text,
 // and an ID attribute with the key value. An event listener is added to each button element to listen for clicks,
@@ -69,6 +74,115 @@ keyboardKeys.forEach(keyboardKey => {
   const keyboardButton = document.createElement('button')
   keyboardButton.textContent = keyboardKey
   keyboardButton.setAttribute('id', keyboardKey)
-  keyboardButton.addEventListener('click', buttonClick)
+  keyboardButton.addEventListener('click', () => buttonClick(keyboardKey))
   keyboardLayout.append(keyboardButton)
 });
+
+const buttonClick = (letter) => {
+  console.log('clicked', letter)
+  if (letter === 'DEL') {
+    deletingLetter();
+    console.log('inputRows', inputRows)
+    return;
+  }
+  if (letter === 'ENTER') {
+    checkGuess();
+    console.log('inputRows', inputRows)
+    return;
+  }
+  addingLetter(letter)
+  console.log('inputRows', inputRows)
+
+}
+
+const addingLetter = (letter) => {
+  if (activeTile < 5 && activeTile < 6) {
+    const tile = document.getElementById('inputRow-' + activeRow + '-tiles-' + activeTile)
+    tile.textContent = letter
+    inputRows[activeRow][activeTile] = letter
+    tile.setAttribute('data', letter)
+    activeTile++
+  }
+}
+
+const deletingLetter = () => {
+  if (activeTile > 0) {
+    activeTile--
+    const tile = document.getElementById('inputRow-' + activeRow + '-tiles-' + activeTile)
+    tile.textContent = ''
+    inputRows[activeRow][activeTile] = ''
+    tile.setAttribute('data', '')
+  }
+}
+
+const checkGuess = () => {
+  const guess = inputRows[activeRow].join('');
+
+  if (activeTile === 5) {
+    console.log('guess is ' + guess, 'word is ' + word);
+    flippingTiles()
+    if (word == guess) {
+      displayMessage('You guessed the word!');
+      endGame = true;
+      return;
+
+    } else {
+      if (activeRow >= 5) {
+        endGame = false;
+        displayMessage('Game Over!');
+        return;
+      }
+      if (activeRow < 5) {
+        activeRow++
+        activeTile = 0
+      }
+    }
+  }
+}
+
+const displayMessage = (message) => {
+  const messageElement = document.createElement('p');
+  messageElement.textContent = message;
+  msgDisplay.append(messageElement);
+  setTimeout(() => msgDisplay.removeChild(messageElement), 2000);
+}
+
+const addKeyboardColour = (keyLetter, colour) => {
+  const keyColour = document.getElementById(keyLetter);
+  keyColour.classList.add(colour);
+}
+
+const flippingTiles = () => {
+  const singleTiles = document.querySelector('#inputRow-' + activeRow).childNodes;
+  let checkWord = word;
+  const guesses = [];
+
+  singleTiles.forEach(tile => {
+    guesses.push({ letter: tile.getAttribute('data'), colour: 'grey-tile' })
+  })
+
+  guesses.forEach((guesses, index) => {
+    if (guesses.letter == word[index]) {
+      guesses.colour = 'green-tile'
+      checkWord = checkWord.replace(guesses.letter, '')
+    }
+  })
+
+  guesses.forEach(guesses => {
+    if (checkWord.includes(guesses.letter)) {
+      guesses.colour = 'yellow-tile'
+      checkWord = checkWord.replace(guesses.letter, '')
+    }
+  })
+  console.log('guess', guesses)
+
+  singleTiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add('flip')
+      tile.classList.add(guesses[index].colour);
+      addKeyboardColour(guesses[index].letter, guesses[index].colour);
+
+    }, 500 * index);
+
+  });
+}
