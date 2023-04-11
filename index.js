35b5f07d2881edcf00a10708d47445fd1faae1bf
ShaliaRamiah <shalia.r20@gmail.com>
2023-04-11 19:26:13 +0200

    fixed unidentified error

--- index.js
+++ index.js
@@ -4,52 +4,50 @@ const express = require("express")
 const cors = require("cors")
 require('dotenv').config()
 
-const  wordle = express()
+const wordle = express()
 
 wordle.use(cors())
 
 
 wordle.get('/word', (req, res) => {
-    const options = {
-        method: 'GET',
-        url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
-        params: {count: '5', wordLength: '5'},
-        headers: {
-          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-          'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
-        }
-      }
+  const options = {
+    method: 'GET',
+    url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
+    params: { count: '5', wordLength: '5' },
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+    }
+  }
 
-      axios.request(options).then((response) => {
-        console.log(response.data)
-        res.json(response.data[0])
-    }).catch((error) => {
-        console.error(error);
-    })
+  axios.request(options).then((response) => {
+    console.log(response.data)
+    res.json(response.data[0])
+  }).catch((error) => {
+    console.error(error);
+  })
 
 })
 
 
 wordle.get('/check', (req, res) => {
-    const word = req.query.word
+  const word = req.query.word
 
-    const options = {
-        method: 'GET',
-        url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/association/',
-        params: {entry: word},
-        headers: {
-          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-          'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
-        }
-      };
-    
-
-axios.request(options).then((response) => {
+  const options = {
+    method: 'GET',
+    url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/association/',
+    params: { entry: word },
+    headers: {
+      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+      'X-RapidAPI-Host': 'twinword-word-graph-dictionary.p.rapidapi.com'
+    }
+  }
+  axios.request(options).then((response) => {
     console.log(response.data)
     res.json(response.data.result_msg)
-}).catch((error) => {
+  }).catch((error) => {
     console.error(error)
-})
+  })
 })
 
 
--- wordle.js
+++ wordle.js
@@ -5,16 +5,17 @@ const msgDisplay = document.querySelector('.message-container');
 
 let word
 
-
+// This function fetches a word from a server running on localhost:8000 and sets it to uppercase
 const getWord = () => {
+  // The fetch function sends a GET request to the specified URL and returns a promise that resolves to the response object
   fetch('http://localhost:8000/word')
-  .then(response => response.json())
-  .then(json => {
+    .then(response => response.json())
+    .then(json => {
       word = json.toUpperCase()
     })
     .catch(err => console.log(err))
 }
-
+// Call the getWord function to fetch and process the word
 getWord();
 
 // creating an array 'keyboardKeys' that stores strings of letters, ENTER, and DEL.
@@ -94,134 +95,153 @@ keyboardKeys.forEach(keyboardKey => {
 });
 
 
-
+//button clicks
 const buttonClick = (letter) => {
-  if(!endGame){
+  if (!endGame) {
+    // If the clicked button is "DEL", removing the last letter from the current guess.
     if (letter === 'DEL') {
       deletingLetter();
       return;
     }
+    // If the clicked button is "ENTER", checking if the current guess is correct.
     if (letter === 'ENTER') {
       checkGuess();
       return;
     }
+    // If the clicked button is a letter, adding it to the current guess.
     addingLetter(letter)
-  
+
   }
 }
 
 
-
+//adding a letter to a tile 
 const addingLetter = (letter) => {
-  if (activeTile < 5 && activeTile < 6) {
+  // Checking if the current active tile is less than 5
+  if (activeTile < 5) {
     const tile = document.getElementById('inputRow-' + activeRow + '-tiles-' + activeTile)
     tile.textContent = letter
     inputRows[activeRow][activeTile] = letter
     tile.setAttribute('data', letter)
+    // Incrementing the activeTile counter for the next letter to be added.
     activeTile++
   }
 }
 
 
-
+//deleting a letter from a tile
 const deletingLetter = () => {
   if (activeTile > 0) {
+    // Decrementing the active tile index by 1
     activeTile--
     const tile = document.getElementById('inputRow-' + activeRow + '-tiles-' + activeTile)
     tile.textContent = ''
     inputRows[activeRow][activeTile] = ''
+    // Setting the 'data' attribute of the tile element to an empty string
     tile.setAttribute('data', '')
   }
 }
 
 
-
+//checking each guess 
 const checkGuess = () => {
+  // Getting the user's guess by joining the input letters in the active row.
   const guess = inputRows[activeRow].join('')
-
+  // If the user has filled all five tiles in a row:
   if (activeTile > 4) {
-
-
+    //i make a request to the local server to check if the guess is a valid word.
     fetch(`http://localhost:8000/check/?word=${guess}`)
-            .then(response => response.json())
-            .then(json => {
-                if (json == 'Entry word not found') {
-                    showMessage('word not in list')
-                    return
-                } else {
+      .then(response => response.json())
+      .then(json => {
+
+        // If the guess is not a valid word:
+        if (json == 'Entry word not found') {
+          displayMessage('word not in list')
+          return
+        } else {
+          //to display the guess in console
+          console.log('guess is ' + guess, 'word is ' + word);
           flippingTiles()
+
+          // If the guess matches the word:
           if (word == guess) {
             displayMessage('You guessed the word!')
             endGame = true;
             return;
           } else {
+
+            // If the guess does not match the word and the game is over:
             if (activeRow >= 5) {
               endGame = true;
               displayMessage('Game Over!')
               return;
             }
             if (activeRow < 5) {
+              // Move to the next row if there are more rows left.
               activeRow++
               activeTile = 0
             }
           }
         }
-      
+
       }).catch(err => console.log(err))
   }
 
-      
 
 
 
-const displayMessage = (message) => {
-  const messageElement = document.createElement('p')
-  messageElement.textContent = message
-  msgDisplay.append(messageElement)
-  setTimeout(() => msgDisplay.removeChild(messageElement), 2000)
-}
+  //display message 
+  const displayMessage = (message) => {
+    const messageElement = document.createElement('p')
+    messageElement.textContent = message
+    msgDisplay.append(messageElement)
+    setTimeout(() => msgDisplay.removeChild(messageElement), 2000)
+  }
 
 
 
-const addKeyboardColour = (keyLetter, colour) => {
-  const key = document.getElementById(keyLetter)
-  key.classList.add(colour)
-}
+  const addKeyboardColour = (keyLetter, colour) => {
+    const key = document.getElementById(keyLetter)
+    key.classList.add(colour)
+  }
 
 
-
-const flippingTiles = () => {
-  const singleTiles = document.querySelector('#inputRow-' + activeRow).childNodes
-  let checkWord = word
-  const guesses = []
-
-  singleTiles.forEach(tile => {
-    guesses.push({ letter: tile.getAttribute('data'), colour: 'grey-tile' })
-  })
-
-  guesses.forEach((guesses, index) => {
-    if (guesses.letter == word[index]) {
-      guesses.colour = 'green-tile'
-      checkWord = checkWord.replace(guesses.letter, '')
-    }
-  })
+  //adding colour to the tiles after a user's guess
+  const flippingTiles = () => {
+    const singleTiles = document.querySelector('#inputRow-' + activeRow).childNodes
+    let checkWord = word
+    const guesses = []
 
 
-  guesses.forEach(guesses => {
-    if (checkWord.includes(guesses.letter)) {
-      guesses.colour = 'yellow-tile'
-      checkWord = checkWord.replace(guesses.letter, '')
-    }
-  })
+    singleTiles.forEach(tile => {
+      guesses.push({ letter: tile.getAttribute('data'), colour: 'grey-tile' })
+    })
+
+    // Looping through the guessed letters and update their colours if they match the word.
+    guesses.forEach((guesses, index) => {
+      if (guesses.letter == word[index]) {
+        guesses.colour = 'green-tile'
+        checkWord = checkWord.replace(guesses.letter, '')
+      }
+    })
+
+    // Looping through the guessed letters and update their colours if they are in the word but not in the right position.
+    guesses.forEach(guesses => {
+      if (checkWord.includes(guesses.letter)) {
+        guesses.colour = 'yellow-tile'
+        checkWord = checkWord.replace(guesses.letter, '')
+      }
+    })
 
 
-  singleTiles.forEach((tile, index) => {
-    setTimeout(() => {
-      tile.classList.add('flip')
-      tile.classList.add(guesses[index].colour);
-      addKeyboardColour(guesses[index].letter, guesses[index].colour);
-    }, 500 * index)
+    singleTiles.forEach((tile, index) => {
+      setTimeout(() => {
+        tile.classList.add('flip')
+        tile.classList.add(guesses[index].colour);
+        // Adding the corresponding colour class to the keyboard key.
+        addKeyboardColour(guesses[index].letter, guesses[index].colour);
+      }, 500 * index)
 
-  });
-}
+    });
+  }
 }
